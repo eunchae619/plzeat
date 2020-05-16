c3c0fa4e413e32c4b758e82ffeefba81b638224d
eunchae619 <eunchae619@naver.com>
2020-05-16 14:35:06 +0900

    존재하는 식자재 error 줄맞게 정렬

--- foods/my_validator.py
+++ foods/my_validator.py
@@ -1,15 +1,2 @@
-from . import models as foods_models
-from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
-
-
-def my_validator(value):
-    # my_food = foods_models.Food.objects.filter(user=value.user)
-    print(value)
-    print(value)
-    print(value)
-    print(value)
-    if value in my_food:
-        raise ValidationError(
-            _("이미있음"), params={"value": value},
-        )
+def my_validator():
+    pass
