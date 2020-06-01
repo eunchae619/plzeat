57f921b2bebe5c6d1965a27a476ef73f4259ad0a
eunchae619 <eunchae619@naver.com>
2020-06-01 16:41:04 +0900

    -

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -24,6 +26,7 @@ urlpatterns = [
     path("foods/", include("foods.urls", namespace="foods")),
     path("recipies/", include("recipies.urls", namespace="recipies")),
     path("users/", include("users.urls", namespace="users")),
+    url(r"media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT,}),
 ]
 
 if settings.DEBUG:
